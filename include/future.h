188bc4f2e13b8d87bff3975ebbd5e9635c5af3c0
Giel van Schijndel <me@mortis.eu>
2017-04-11 15:09:13 +0200

    Use boost::optional instead of rolling our own

--- include/future.h
+++ include/future.h
@@ -1,40 +1,27 @@
 #ifndef FUTURE_H
 #define FUTURE_H
 
+#include <boost/optional.hpp>
 #include <future>
 #include <memory>
-#include <type_traits>
 
 template <typename T>
 class result {
 public:
   result(result&& rhs) = delete;
   result& operator=(result&& rhs) = delete;
-  ~result()
-  {
-    if (promise_made) {
-      promise_ref().~promise<T>();
-    }
-  }
+
   std::future<T> get_local() {
-    if (!promise_made) {
-      new (&storage) std::promise<T>();
-      promise_made = true;
+    if (!promise) {
+      promise = std::promise<T>();
       // TODO: signal to the remote that we actually wish to receive the value associated with this.
     }
 
-    return promise_ref().get_future();
+    return promise->get_future();
   }
 
 private:
-  std::promise<T>& promise_ref()
-  {
-    return *reinterpret_cast<std::promise<T>*>(&storage);
-  }
-
-private:
-  typename std::aligned_storage<sizeof(std::promise<T>), std::alignment_of<std::promise<T>>::value>::type storage;
-  bool promise_made = false;
+  boost::optional<std::promise<T>> promise;
 };
 
 template <typename T>
