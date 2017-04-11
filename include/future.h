ee70dd23c093f44c408400531b2099b025c4af39
Giel van Schijndel <me@mortis.eu>
2017-04-11 18:09:23 +0200

    Make rpc::handle::result type-erased deserializable

--- include/future.h
+++ include/future.h
@@ -4,24 +4,83 @@
 #include <boost/optional.hpp>
 #include <future>
 #include <memory>
+#include "serializer.h"
+
+struct Serializer;
+
+// TODO: add an actual implementation of this thing
+template <typename T, typename E>
+struct expected;
 
 template <typename T>
-class result {
+struct handle_promise {
+  typedef std::promise<T> promise_type;
+
+  void operator()(promise_type& value, Serializer& s) const {
+      value.set_value(reader<T>::read(s));
+  }
+};
+
+template <>
+struct handle_promise<void> {
+  typedef std::promise<void> promise_type;
+  void operator()(promise_type& value, Serializer& ) const {
+    value.set_value();
+  }
+};
+
+template <typename T, typename E>
+struct handle_promise<expected<T, E>> {
+  typedef std::promise<T> promise_type;
+  void operator()(promise_type& value, Serializer& s) const {
+    auto e = reader<expected<T, E>>::read(s);
+    if (e)
+      value.set_value(*e);
+    else
+      value.set_error(e.error());
+  }
+};
+
+template <typename E>
+struct handle_promise<expected<void, E>> {
+  typedef std::promise<void> promise_type;
+  void operator()(promise_type& value, Serializer& s) const {
+    auto e = reader<expected<void, E>>::read(s);
+    if (e)
+      value.set_value();
+    else
+      value.set_exception(e.error());
+  }
+};
+
+struct result_base
+{
+  virtual void deserialize_from(Serializer&) = 0;
+};
+
+template <typename T>
+class result : public result_base {
 public:
   result(result&& rhs) = delete;
   result& operator=(result&& rhs) = delete;
 
   std::future<T> get_local() {
     if (!promise) {
-      promise = std::promise<T>();
+      promise = promise_type();
       // TODO: signal to the remote that we actually wish to receive the value associated with this.
     }
 
     return promise->get_future();
   }
 
+  void deserialize_from(Serializer& deserializer) override
+  {
+    handle_promise<T>()(*promise, deserializer);
+  }
+
 private:
-  boost::optional<std::promise<T>> promise;
+  typedef typename handle_promise<T>::promise_type promise_type;
+  boost::optional<promise_type> promise;
 };
 
 template <typename T>
