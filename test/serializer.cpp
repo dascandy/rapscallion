5a7824e6fc3d72ee14c54d60a5b36ab7e19d846e
Giel van Schijndel <me@mortis.eu>
2017-04-12 23:49:55 +0200

    float serialize test: add size expectations

--- test/serializer.cpp
+++ test/serializer.cpp
@@ -3,21 +3,37 @@
 #include <limits>
 #include <serializer.h>
 
-void test(const double in) {
+void test(const double in, const std::ptrdiff_t expected_size = -1) {
   Serializer s;
   writer<decltype(+in)>::write(s, in);
-  const auto buf = s.data();
-  Deserializer d(s.buffer, s.buffer.size() - buf.second);
+  Deserializer d(s.buffer, s.buffer.size() - s.data().second);
+
+  const auto size = d.end - d.ptr;
+  if (expected_size >= 0)
+    assert(size == expected_size);
+
+  // largest encoding of IEEE754 binary64 float:
+  //  (11 exponent bits + exponent sign + NaN/inf flag) = 13 bits / 7 bit/byte = 2 byte
+  //  (52 bit fraction + sign) = 53 bits / 7 bit/byte = 8 byte
+  // + = 10 byte
+  assert(size <= 10);
+
   const auto out = reader<decltype(+in)>::read(d);
   assert(in == out || (std::isnan(in) && std::isnan(out)));
 }
 
 int main() {
-  test(std::numeric_limits<double>::infinity());
-  test(-std::numeric_limits<double>::infinity());
-  test(std::numeric_limits<double>::quiet_NaN());
-  test(0.0);
-  test(-0.0);
+  // Smallest encodings, only requiring flags
+  test(std::numeric_limits<double>::infinity(), 2);
+  test(-std::numeric_limits<double>::infinity(), 2);
+  test(std::numeric_limits<double>::quiet_NaN(), 2);
+  test(0.0, 2);
+  test(-0.0, 2);
+
+  test(0.25);
+  test(-0.25);
+  test(0.5);
+  test(-0.5);
   test(1.0);
   test(-1.0);
   test(2.0);
