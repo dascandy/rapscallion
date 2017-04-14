432ab21dc1e9284c64d7bf27a18a314c949e0d0f
Giel van Schijndel <me@mortis.eu>
2017-04-14 14:42:15 +0200

    Move Serializer and Deserializer to namespaces of its own
    To allow adding some RPC specific methods discoverable through ADL.

---
+++ include/RaPsCallion/serializer_fwd.hpp
@@ -0,0 +1,19 @@
+#ifndef SERIALIZER_FWD_HPP
+#define SERIALIZER_FWD_HPP
+
+namespace rapscallion {
+
+namespace Serializer_ADL {
+struct Serializer;
+}
+
+using Serializer_ADL::Serializer;
+
+namespace Deserializer_ADL {
+struct Deserializer;
+}
+using Deserializer_ADL::Deserializer;
+
+}
+
+#endif
