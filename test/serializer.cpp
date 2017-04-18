f2e8249c0106c4643ab5d9a715b15fd8dcf6362d
Giel van Schijndel <me@mortis.eu>
2017-04-18 16:27:32 +0200

    Add serialization format expectations to test
    To pin the format and to document the requirement that we want floats
    containing just the (implicit) one in the fraction to encode with a 0x01
    byte for its fraction.

--- test/serializer.cpp
+++ test/serializer.cpp
@@ -6,23 +6,68 @@
 namespace rapscallion {
 namespace test {
 
-void test(const std::string& inStr, const double in, const std::ptrdiff_t expected_size = -1) {
+struct byte_view
+{
+  constexpr byte_view() = default;
+
+  byte_view(const Deserializer& d)
+    : first(reinterpret_cast<const char*>(d.ptr))
+    , last (reinterpret_cast<const char*>(d.end))
+  {}
+
+  template <size_t N>
+  constexpr byte_view(const char (&arr)[N])
+    : first(arr)
+    , last(&arr[N - 1])
+  {}
+
+  constexpr const char* begin() const { return first;         }
+  constexpr const char* end()   const { return last;          }
+  constexpr size_t      size()  const { return last - first;  }
+  constexpr bool        empty() const { return first == last; }
+
+private:
+  const char* first = nullptr;
+  const char* last  = nullptr;
+};
+
+bool operator==(const byte_view& lhs, const byte_view& rhs) {
+  return lhs.size() == rhs.size() && std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+std::ostream& operator<<(std::ostream& os, const byte_view& v) {
+  const auto oldFlags = os.setf(std::ios_base::hex, std::ios_base::basefield);
+  const auto oldFill = os.fill('0');
+  os << '"';
+  for (const auto& c : v) {
+    os << "\\x" << std::setw(2) << static_cast<unsigned>(static_cast<unsigned char>(c));
+  }
+  os << '"';
+  os.setf(oldFlags);
+  os.fill(oldFlags);
+  return os;
+}
+
+void test(const std::string& inStr, const double in, const std::ptrdiff_t expected_size = -1, const byte_view& expected_serialization = byte_view()) {
   GIVEN("the real number " + inStr) {
     WHEN("we serialize it") {
       Serializer s;
       serializer<decltype(+in)>::write(s, in);
       Deserializer d(s);
 
-      THEN("the output meets size expectations") {
-        const auto size = d.end - d.ptr;
+      THEN("the serialized output meets expectations") {
+        const byte_view serialized(d);
         if (expected_size >= 0)
-          CHECK(size == expected_size);
+          CHECK(serialized.size() == expected_size);
 
         // largest encoding of IEEE754 binary64 float:
         //  (11 exponent bits + exponent sign + NaN/inf flag) = 13 bits / 7 bit/byte = 2 byte
         //  (52 bit fraction + sign) = 53 bits / 7 bit/byte = 8 byte
         // + = 10 byte
-        CHECK(size <= 10);
+        CHECK(serialized.size() <= 10);
+
+        if (!expected_serialization.empty())
+          CHECK(serialized == expected_serialization);
 
         AND_WHEN("we deserialize it again") {
           const auto out = serializer<decltype(+in)>::read(d);
@@ -40,6 +85,10 @@ void test(const std::string& inStr, const double in, const std::ptrdiff_t expect
   }
 }
 
+void test(const std::string& inStr, const double in, const byte_view& expected_serialization) {
+  return test(inStr, in, expected_serialization.size(), expected_serialization);
+}
+
 }
 }
 
@@ -51,26 +100,27 @@ SCENARIO("Serializing floating point numbers", "[serialization]") {
   test(STRIFY1ST(__VA_ARGS__, void), __VA_ARGS__)
 
   // Smallest encodings, only requiring flags
-  TEST(std::numeric_limits<double>::infinity(), 1);
-  TEST(-std::numeric_limits<double>::infinity(), 1);
-  TEST(std::numeric_limits<double>::quiet_NaN(), 1);
-  TEST(0.0, 1);
-  TEST(-0.0, 1);
+  TEST( std::numeric_limits<double>::infinity(), "\x02");
+  TEST(-std::numeric_limits<double>::infinity(), "\x03");
+  TEST(std::numeric_limits<double>::quiet_NaN(), "\x00");
+  TEST( 0.0, "\x04");
+  TEST(-0.0, "\x05");
 
-  TEST(0.03125, 2);
-  TEST(-0.03125, 2);
-  TEST(0.0625, 2);
-  TEST(-0.0625, 2);
-  TEST(0.125, 2);
-  TEST(-0.125, 2);
-  TEST(0.25, 2);
-  TEST(-0.25, 2);
-  TEST(0.5, 2);
-  TEST(-0.5, 2);
-  TEST(1.0, 2);
-  TEST(-1.0, 2);
-  TEST(2.0, 2);
-  TEST(-2.0, 2);
+  TEST( 0.03125, "\x15" "\x01");
+  TEST(-0.03125, "\x17" "\x01");
+  TEST( 0.0625 , "\x11" "\x01");
+  TEST(-0.0625 , "\x13" "\x01");
+  TEST( 0.125  , "\x0d" "\x01");
+  TEST(-0.125  , "\x0f" "\x01");
+  TEST( 0.25   , "\x09" "\x01");
+  TEST(-0.25   , "\x0b" "\x01");
+  TEST( 0.5    , "\x06" "\x01");
+  TEST(-0.5    , "\x08" "\x01");
+  TEST( 1.0    , "\x0a" "\x01");
+  TEST(-1.0    , "\x0c" "\x01");
+  TEST( 2.0    , "\x0e" "\x01");
+  TEST(-2.0    , "\x10" "\x01");
+
   TEST(M_PI, 9);
   TEST(-M_PI, 9);
   TEST((float)M_PI, 5);
